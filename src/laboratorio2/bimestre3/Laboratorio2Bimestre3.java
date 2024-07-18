bfe05e1c11b9c5674ed9315c930b1c75abcad767
Paola Bolaños <paola.bolanos@outlook.es>
2024-07-18 12:36:25 -0600

    Laboratorio2

---
+++ src/laboratorio2/bimestre3/Laboratorio2Bimestre3.java
@@ -0,0 +1,23 @@
+
+package laboratorio2.bimestre3;
+
+public class Laboratorio2Bimestre3 {
+
+    public static void main(String[] args) {
+    int matriz[][]= {{5,7,9},{4,2,1},{1,1,1}}; 
+        for (int i = 0; i <matriz.length; i++) {
+            for (int j = 0; j <matriz.length; j++){   
+                System.out.print(matriz[i][j]+"|");
+     }
+        }
+    }
+}
+   
+                
+            
+    
+    
+  
+            
+
+
